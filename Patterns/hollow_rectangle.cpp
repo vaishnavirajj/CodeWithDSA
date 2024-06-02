0b10a88260bd76c5cc1d61056bb5452706e49767
Vaishnavi Raj <vaishnaviraj950@gmail.com>
2024-06-02 23:49:29 +0530

    initial commit

---
+++ Patterns/hollow_rectangle.cpp
@@ -0,0 +1,24 @@
+//Printing Hollow rectangle
+
+#include<iostream>
+using namespace std;
+int main() {
+    int rows;
+    int cols;
+    cout<<"enter the no of rows "<<endl;
+    cin>>rows;
+    cout<<"enter the no of cols "<<endl;
+    cin>>cols;
+    for (int i=0;i<rows;i++) {
+        for (int j=0;j<cols;j++) {
+            if(i==0 || i==rows-1 || j==0 || j==cols-1) {
+                cout<<"*"<<" ";
+            }
+            else {
+                cout<<" "<<" ";
+            }
+        }
+        cout<<endl;
+    }
+
+}
