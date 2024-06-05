0623c39461d1105375ed3cefd49ce4a84d7f8a72
Vaishnavi Raj <vaishnaviraj950@gmail.com>
2024-06-05 23:05:01 +0530

    initial commit

---
+++ Patterns/Numeric_half_pyramid.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int rows;
+    cin>>rows;
+    for (int i=0; i<rows; i++) {
+        for (int j=0; j<i+1;j++){
+            cout<<j+1<<" ";
+
+}
+        cout<<endl;
+    }
+
+}
