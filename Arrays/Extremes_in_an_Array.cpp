fa891650e74ea5f4e7c0c3f9edb14d494b48e93c
Vaishnavi Raj <vaishnaviraj950@gmail.com>
2024-06-06 12:32:17 +0530

    Initial commit

--- Arrays/Extremes_in_an_Array.cpp
+++ Arrays/Extremes_in_an_Array.cpp
@@ -1,41 +1,32 @@
 #include<iostream>
 using namespace std;
-void printExtremeInAnArray(int arr[],int size){
+void printExtremes(int arr[],int size) {
     int left=0;
     int right=size-1;
-    //printing the extremes of an array
-    
-        while(left<=right) {
-            if (left==right){
-                cout<<arr[left]<<endl;
-            }
-            else {
-                 cout<<arr[left];
-            //cout<<arr[right]<<" ";
-            left++;
-            right--;
-            }
-           
-        }
-    }   
-        
 
+    //Printing the extremes
+    while(left<=right) {
+        if(left==right){
+            cout<<arr[left]<<endl;
+        }
+        else{
+            cout<<arr[left]<<" ";
+            cout<<arr[right]<<" ";
+        }
+        left++;
+        right--;
+    }
+}
 int main() {
     int size;
     cin>>size;
     int arr[size];
-    //Taking input in an array
-    for (int i=0;i<size;i++) {
+    for(int i=0;i<size;i++) {
         cin>>arr[i];
     }
-    //printing the array
-    for (int i=0;i<size;i++) {
-        cout<<arr[i]<<" ";
-    }
     cout<<endl;
-    printExtremeInAnArray(arr,size);
+    printExtremes(arr,size);
 
+
+    
 }
-
-
-// we will do tomorrow
