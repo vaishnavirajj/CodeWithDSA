718ae47d8bc328f3895826fb2845db438190193b
Vaishnavi Raj <121537484+vaishnavirajj@users.noreply.github.com>
2024-06-09 15:35:20 +0530

    Add files via upload

---
+++ Patterns/Hollow_full_Pyramid.cpp
@@ -0,0 +1,24 @@
+//printing hollow pyramid
+
+#include<iostream>
+using namespace std;
+int main() {
+    int rows;
+    cin>>rows;
+    for (int i=0;i<rows;i++){
+        for (int j=0;j<rows-i-1;j++) {
+            cout<<" ";
+         
+    }
+    for (int j=0;j<i+1;j++) {
+        if(j==0 || j==i+1-1 ||i==rows-1)  {
+        cout<<"*";
+    }
+    else {
+        cout<<" ";
+    }
+   cout<<" ";
+}
+
+ cout<<endl;
+}}
