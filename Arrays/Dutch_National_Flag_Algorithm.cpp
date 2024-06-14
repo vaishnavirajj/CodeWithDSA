25661f93dfe40f16d86be64c8440d6e796e4c975
Vaishnavi Raj <121537484+vaishnavirajj@users.noreply.github.com>
2024-06-14 09:29:40 +0530

    Update Initial commit

--- Arrays/Dutch_National_Flag_Algorithm.cpp
+++ Arrays/Dutch_National_Flag_Algorithm.cpp
@@ -1,26 +1,51 @@
 #include<iostream>
+#include<utility>
 using namespace std;
-int main() {
-    int size=14;
-    int arr[size] ={1,0,2,1,0,0,1,2,2,1,0,0,1,2}
+
+void dutchNationalFlagAlgo(int arr[],int size) {
     int left=0;
     int right=size-1;
     int i=0;
-    while (i<=right) {
-        if(arr[i]==0){
+    while(i<=right) {
+        if(arr[i]==0) {
             swap(arr[i],arr[left]);
             i++;
             left++;
-             }
+        }
+
         else if(arr[i]==2) {
             swap(arr[i],arr[right]);
             right--;
-        }
-        else{
-            i++;
+            //no need of i++
         }
 
+        else {
+            i++;
+        }
     }
+
+    //printing the sorted array
+    for (int i=0;i<size;i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+
+
+}
+int main() {
+    int size;
+    cin>>size;
+    int arr[size];
+    for(int i=0;i<size;i++) {
+        cin>>arr[i];
+    }
+    //printing the array
+    for (int i=0;i<size;i++) {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
     
+    cout<<"Sorted array: "<<endl;
+    dutchNationalFlagAlgo(arr,size);
 
 }
