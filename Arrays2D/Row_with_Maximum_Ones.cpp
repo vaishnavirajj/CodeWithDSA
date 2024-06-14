71aba859bae42e4f31cf8d475eaa44aeaac0a0cd
Vaishnavi Raj <121537484+vaishnavirajj@users.noreply.github.com>
2024-06-14 20:13:14 +0530

    Add files via upload

---
+++ Arrays2D/Row_with_Maximum_Ones.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<limits.h>
+using namespace std;
+int main() {
+    int rows=5;
+    int cols=4;
+    int countOne=0;
+    int rowNo= -1;
+    int arr[5][4] = {
+        {1,0,0,0 },
+        {0,1,1,0},
+        {0,1,1,0},
+        {1,1,1,0 },
+        {0,0,1,0}
+    };
+   
+    for (int i=0;i<rows;i++) {
+         int count=0;
+        for (int j=0;j<cols;j++) {
+            if(arr[i][j]==1){
+                count++;
+                
+            }
+}
+if(count>countOne) {
+    countOne=count;
+    rowNo=i;
+} 
+} 
+
+cout<<"row no and maximum number of ones: "<<rowNo<<","<<countOne<<endl;
+
+
+
+}
