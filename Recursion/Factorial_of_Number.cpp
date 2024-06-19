a90aa2e118e34de0c2f375139ad4c7d442540d08
Vaishnavi Raj <121537484+vaishnavirajj@users.noreply.github.com>
2024-06-19 11:45:50 +0530

    Add files via upload

---
+++ Recursion/Factorial_of_Number.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int getFactorial(int n){
+       //Base Condition
+        if(n==0 || n==1) {
+            return 1;
+        }
+        //Recursive call
+        int recursionAns=getFactorial(n-1);
+        //processing
+        int finalAns=n*recursionAns;
+        return finalAns;
+    }
+int main() {
+    cout<<"Enter the number: ";
+    int number;
+    cin>>number;
+    cout<<getFactorial(number);
+
+    
+}
