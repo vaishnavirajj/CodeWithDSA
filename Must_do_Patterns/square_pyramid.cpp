98f3bec500bddb7dfa78e9be86622d89aa35990f
Vaishnavi Raj <121537484+vaishnavirajj@users.noreply.github.com>
2024-07-04 18:16:08 +0530

    Initial commit

---
+++ Must_do_Patterns/square_pyramid.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n; //no of rows
+    cout<<"enter no of rows:";
+    cin>>n;
+
+    for (int i=0;i<n;i++) {
+        for(int j=0;j<n;j++) {
+            cout<<"* ";
+
+        }
+        cout<<endl;
+
+    }
+}
