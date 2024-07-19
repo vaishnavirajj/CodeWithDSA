bec92053ad62f0564a9ed758d4fae9dc5d43f1d4
Vaishnavi Raj <121537484+vaishnavirajj@users.noreply.github.com>
2024-07-19 23:42:08 +0530

    Add files via upload

---
+++ Patterns/Numeric_Palindrome_Equilateral_Pyramid.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int n=5;
+    for (int i=0;i<n;i++) {
+        //spaces
+        for (int j=0;j<n-i-1;j++) {
+            cout<<"  ";
+        }
+
+        for(int j=0;j<=i;j++) {
+            cout<<j+1<<" ";
+        }
+        for(int j=i+1;j<=i;j--) {
+            cout<<j;
+        }
+
+       cout<<endl;
+    }
+}
