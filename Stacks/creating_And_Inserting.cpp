4a9d51f6426afa0ba4ea94cbc9be887c6733f14a
Vaishnavi Raj <121537484+vaishnavirajj@users.noreply.github.com>
2024-08-12 19:57:46 +0530

    Initial commit

--- Stacks/creating_And_Inserting.cpp
+++ Stacks/creating_And_Inserting.cpp
@@ -1,1 +1,22 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main () {
+    //creation
+    stack<int>s;
+    s.push(10);
+    s.push(20);
+    s.push(30);
+    //size 
+    cout<<s.size()<<endl;
+    //check empty or not
+    cout<<s.empty()<<endl;
+    //peek element 
+    cout<<s.top()<<endl;
+    //removal
+    s.pop();
+    cout<<s.top()<<endl;
+    return 0;
 
+
+}
