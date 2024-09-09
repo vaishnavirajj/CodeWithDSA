d716a268f5b62c61ffe8aaadc355caf3dd19a2b1
Vaishnavi Raj <121537484+vaishnavirajj@users.noreply.github.com>
2024-09-09 14:35:26 +0530

    Update deque.cpp

--- C++STL/Deque/deque.cpp
+++ C++STL/Deque/deque.cpp
@@ -1,21 +1,52 @@
+//double ended queue
+// similar to vectors but allow efficient insertion and removal from both end 
+//suitable when elements need to be inserted or removed frequently from the front or back
+
+
+
 #include<iostream>
 #include<deque>
 using namespace std;
 int main () {
-    deque<int>d;
-    d.push_back(1);
-    d.push_back(2);
-    d.push_back(3);
-    for (int i:d) {
-        cout<<i<<" ";
+    deque<int>dq;
+    //insertion fron back
+    dq.push_back(20);
+    //20
+    dq.push_back(30);
+    //20 ,30
+    dq.push_back(40);
+    //20,30,40
+    dq.push_back(50);
+    //20,30,40,50
+    //insertion from front
+    dq.push_front(60);
+    //60,20,30,40,50
+    dq.push_front(70);
+    //70,60,20,30,40,50
+
+    //removing from front
+    dq.pop_front();
+    //60,50,20,30,40,50
+
+    //removing from back
+    dq.pop_back();
+    //60,50,40,30,40
+
+    cout<<"Size: "<<dq.size() <<endl;
+
+    cout<<"Front element: "<<dq.front() <<endl;
+    cout<<"back element: "<<dq.back() <<endl;
+
+    //traversing
+    deque<int>::iterator it=dq.begin();
+    while(it != dq.end()) {
+        cout<< *it <<" ";
+        it++;
     }
     cout<<endl;
-    cout<<"size of d---> "<<d.size()<<endl;
-    d.pop_back();
-    cout<<"size of d---> "<<d.size()<<endl;
-    cout<<"empty or not---> "<<d.empty()<<endl;
-    d.erase(d.begin(),d.end());
-    cout<<"size of d after erasing all elements---> "<<d.size()<<endl;
+
+
+
    
 
 }
