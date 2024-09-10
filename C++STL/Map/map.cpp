559a4da8cbd64e1a7a703ab2517b4ca3fda4aee9
Vaishnavi Raj <121537484+vaishnavirajj@users.noreply.github.com>
2024-09-10 14:01:46 +0530

    Update map.cpp

--- C++STL/Map/map.cpp
+++ C++STL/Map/map.cpp
@@ -2,8 +2,97 @@
 //values can be same but key will be unique
 #include<iostream>
 #include<map>
+#include<unordered_map>
 using namespace std;
 int main() {
+    //creation
+    unordered_map<string, string>table;
+
+    //insertion 1
+    table["in"] ="India";
+
+    //insertion 2
+    table.insert(make_pair("en","England"));
+
+    //insertion 3
+    pair<string, string> p;
+    p.first="br";
+    p.second="brazil";
+    table.insert(p);
+
+    cout<<"Size: "<<table.size()<<endl;
+
+    if(table.empty()==true) {
+        cout<<"table is empty" <<endl;
+    }
+
+    else {
+        cout<<"table is not empty" <<endl;
+    }
+    
+
+    unordered_map<string ,string>::iterator it=table.begin();
+    while (it != table.end( )) {
+        pair<string ,string>p = *it;
+        cout<<p.first<<" "<<p.second<<endl;
+        it++;
+    }
+
+    if(table.find("in") != table.end()) {
+        cout<<"key found"<<endl;
+
+    }
+    else{
+      cout<<"key not  found"<<endl;
+    }
+
+    if(table.count("in") == 1) {
+        cout<<"Key found"<<endl;
+    }
+    if(table.count("in") == 0) {
+        cout<<"key not found"<<endl;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     map<int,string>m;
     m[1]="vaishnavi";
     m[2]="raj";
